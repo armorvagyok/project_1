8de8024a6a1ba407e61f19e90bea9f5b7ae67dff
armorvagyok <fehertamas05@gmail.com>
2024-04-07 17:43:41 +0200

    menu wrapper

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -2,12 +2,11 @@ export default function Menu() {
     return(
         <>
         <br/>
-        <h1 className="pagetitle"><b>musical society</b></h1>
-        <a className="homebt" href={`/`}><b>Home</b></a>
-        <a className="postbt" href={`/posts`} color="#00B86B"><b>Posts</b></a>
-        <a className="profilebt" href={`/profile`}><b>Profile</b></a>
-        <a className="loginbt" href={`/login`}><b>Login</b></a>
-
+        <h1 className="pagetitle titlewrapper"><b>musical society</b></h1>
+        <a className="homebt buttonwrapper" href={`/`}><b>Home</b></a>
+        <a className="postbt buttonwrapper" href={`/posts`} color="#00B86B"><b>Posts</b></a>
+        <a className="profilebt buttonwrapper" href={`/profile`}><b>Profile</b></a>
+        <a className="loginbt buttonwrapper" href={`/login`}><b>Login</b></a>
         <br/>
         </>
     )
