e83fa658b9549c72f5fe9607c191f82efa23bb6a
armorvagyok <fehertamas05@gmail.com>
2024-04-18 13:01:17 +0200

    kis update

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,19 +1,25 @@
-import axios from "axios"
-import Router from "next/router"
+import axios, { AxiosError } from "axios"
+import { useRouter } from "next/router"
+import { Form } from "react-bootstrap";
 
 export default function Menu() {
+    const router = useRouter();
+
     return(
+        
         <>
             <br/>
             <h1 className="pagetitle titlewrapper"><b>NOTEHOUSE</b></h1>
             <a className="searchbt buttonwrapper" href={`/search`} color="#00B86B"><b>Search</b></a>
             <a className="postbt buttonwrapper" href={`/`} color="#00B86B"><b>Posts</b></a>
             <a className="profilebt buttonwrapper" href={`/profile`}><b>Profile</b></a>
-            <a href="#" className="logoutbt buttonwrapper" onClick={() => {
+            <Form.Control className="logoutbt buttonwrapper" onSubmit={() => {
                 axios.post("http://localhost/project_1/server/public/api/logout").then((res) => {
-                    Router.push("/login")
+                    router.push("/login")
+                }).catch((AxiosError) => {
+                    alert("Error")
                 })
-            }}><b>Logout</b></a>
+            }}><b>Logout</b></Form.Control>
             <br/>
         </>
     )
